8112d862a370f373b45a2ad008945458610c30e7
tina513 <dlnaz.ai@hotmail.com>
2016-08-12 18:34:40 -0400

    html superhero mark up initial

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const app = {
-  host: 'https://survey-market-api.herokuapp.com'
+  //host: 'https://survey-market-api.herokuapp.com'
+  host: 'http://localhost:3000'
 };
 
 module.exports = app;
--- assets/scripts/takesurvey/surveyFetch.js
+++ assets/scripts/takesurvey/surveyFetch.js
@@ -4,7 +4,8 @@ const app = require('../app.js');
 
 const getASurvey = (id) => {
   if(app.user === undefined) {
-    $('#error-message').show();
+    // $('#error-message').show();
+    $("#error-message").slideDown("slow");
   }
   return $.ajax({
     url: app.host + '/surveys/' + id,
