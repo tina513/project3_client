e4a5ef13b77e05bba31a0e74e9402e1cc5e6ebd1
tina513 <dlnaz.ai@hotmail.com>
2016-08-13 13:24:26 -0400

    heroku url got changed

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const app = {
-  //host: 'https://survey-market-api.herokuapp.com'
-  host: 'http://localhost:3000'
+  host: 'https://shrouded-plains-28686.herokuapp.com/'
+  //host: 'http://localhost:3000'
 };
 
 module.exports = app;
