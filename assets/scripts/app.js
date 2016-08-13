f342636106a70d2ff5ac627f4dd6e8b9338cdd0f
tina513 <dlnaz.ai@hotmail.com>
2016-08-13 13:26:24 -0400

    changing heroku url

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const app = {
-  host: 'https://shrouded-plains-28686.herokuapp.com/'
+  host: 'https://shrouded-plains-28686.herokuapp.com'
   //host: 'http://localhost:3000'
 };
 
