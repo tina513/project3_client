62273fc1f18bea0dacc450b502e4df299525b8a1
tina513 <dlnaz.ai@hotmail.com>
2016-08-15 15:18:21 -0400

    add back bundle

--- bundle.js
+++ bundle.js
@@ -222,7 +222,8 @@ webpackJsonp([0],[
 	'use strict';
 
 	var app = {
-	  host: 'https://quiet-savannah-87596.herokuapp.com/'
+	  host: 'https://survey-market-api.herokuapp.com/'
+	  // host: 'https://quiet-savannah-87596.herokuapp.com/'
 	  //host: 'http://localhost:3000'
 	};
 
