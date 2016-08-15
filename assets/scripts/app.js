f8ea31e4210b6ecbddd046ecb5328d4674c67ea9
tina513 <dlnaz.ai@hotmail.com>
2016-08-15 15:17:28 -0400

    change a bit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const app = {
-  host: 'https://quiet-savannah-87596.herokuapp.com/'
+  host: 'https://survey-market-api.herokuapp.com/'
+  // host: 'https://quiet-savannah-87596.herokuapp.com/'
   //host: 'http://localhost:3000'
 };
 
