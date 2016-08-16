4dfffc80c27f4a64918b73f499a0411add3304f3
tina513 <dlnaz.ai@hotmail.com>
2016-08-16 10:00:14 -0400

    change backend url

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const app = {
-  host: 'https://quiet-mesa-89038.herokuapp.com/'
-  // host: 'https://quiet-savannah-87596.herokuapp.com/'
+  host: 'https://quiet-mesa-89038.herokuapp.com'
+  // host: 'https://quiet-savannah-87596.herokuapp.com'
   //host: 'http://localhost:3000'
 };
 
